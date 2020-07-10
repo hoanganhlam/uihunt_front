b68f5050985334bb81e8e5671e598e2aa3f8e4b4
lamha <lam@trip.vn>
2020-07-10 23:01:09 +0700

    INIT

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <header class="">
+            <div class="container">
+                <nav class="navbar header" role="navigation" aria-label="main navigation">
+                    <div class="navbar-brand">
+                        <n-link class="navbar-item" to="/">UIHUNT</n-link>
+                        <div class="navbar-burger">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                    <div id="navbarBasicExample" class="navbar-menu">
+                        <div class="navbar-start">
+                            <a class="navbar-item">Topic</a>
+                            <a class="navbar-item">Inspiration</a>
+                        </div>
+                        <div class="navbar-end">
+                            <div class="navbar-item">
+                                <login-bar/>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <main class="main-content">
+            <nuxt/>
+        </main>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+
+module.exports = {
+    mode: 'universal',
+    /*
+    ** Headers of the page
+    */
+    head: {
+        titleTemplate: '%s - UIHunt.com',
+        title: process.env.npm_package_name || '',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ]
+    },
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: {color: '#fff'},
+    /*
+    ** Global CSS
+    */
+    css: [
+        '@/scss/app.scss'
+    ],
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [
+        '@/components',
+        '@/plugins/auth',
+        '@/plugins/axios',
+        '@/plugins/mixins',
+        {src: '~/plugins/repository'},
+    ],
+    /*
+    ** Nuxt.js dev-modules
+    */
+    buildModules: [],
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
+        // Doc: https://buefy.github.io/#/documentation
+        'nuxt-buefy',
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+        // Doc: https://github.com/nuxt-community/dotenv-module
+        '@nuxtjs/dotenv',
+        ['cookie-universal-nuxt', {alias: 'ck'}],
+    ],
+    /*
+    ** Axios module configuration
+    ** See https://axios.nuxtjs.org/options
+    */
+    axios: {
+        baseURL: process.env.API_DOMAIN + '/v1'
+    },
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** You can extend webpack config here
+        */
+        extend(config, ctx) {
+        }
+    },
+    vue: {
+        config: {
+            productionTip: false,
+            devtools: true
+        }
+    }
+};
